8f2edc7855feb4f4f8fc93d8f5f0250d23c81d52
Trisdub <trisdubois@icloud.com>
2025-09-28 11:41:08 -0400

    Update and rename _config.yml to config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+const CONFIG = {
+  formLink: "https://docs.google.com/forms/d/e/1FAIpQLScM7Pyn9Udc0EZcG2yJ-YpqCVqny-dqTNXOGwYQSyEyGS1Nwg/viewform?usp=sharing&ouid=111050342805730174946",
+  sheetUrl: "https://docs.google.com/spreadsheets/d/1UNuPz22hDFkdUjWJQWkeornQHfaWtsXhYzX5g8g_Mvg/edit?usp=sharing"
+};
