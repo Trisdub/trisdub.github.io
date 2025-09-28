b648b46e1faf30aba4a1eb74b1757160f9c8080d
Trisdub <trisdubois@icloud.com>
2025-09-28 12:54:18 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 const CONFIG = {
   formLink: "https://docs.google.com/forms/d/e/1FAIpQLScM7Pyn9Udc0EZcG2yJ-YpqCVqny-dqTNXOGwYQSyEyGS1Nwg/viewform?usp=sharing&ouid=111050342805730174946",
-  sheetUrl: "https://docs.google.com/spreadsheets/d/1UNuPz22hDFkdUjWJQWkeornQHfaWtsXhYzX5g8g_Mvg/edit?usp=sharing"
+  sheetUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQAWka7mgkzgc4-ijO-luKphGZj4zWNSzGbe-tFBORU1_20nz92foR27ZXeYh-UUgzQsoaCapFYnbVX/pub?output=csv"
 };
